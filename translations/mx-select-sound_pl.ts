e5a0b987553fcb26d5af501a91471d407cc5b846
oldgizahub <geoffgigg@hotmail.com>
2019-08-26 20:25:34 -0400

    Translation refresh for MX-19

--- translations/mx-select-sound_pl.ts
+++ translations/mx-select-sound_pl.ts
@@ -1,143 +1,141 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="pl">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
 <context>
     <name>mxselectsound</name>
     <message>
-        <location filename="../mxselectsound.ui" line="14"/>
-        <location filename="../mxselectsound.cpp" line="48"/>
-        <location filename="../mxselectsound.cpp" line="78"/>
-        <location filename="../mxselectsound.cpp" line="136"/>
-        <location filename="../mxselectsound.cpp" line="148"/>
-        <location filename="../mxselectsound.cpp" line="182"/>
-        <location filename="../mxselectsound.cpp" line="191"/>
+        <location filename="mxselectsound.ui" line="14"/>
+        <location filename="mxselectsound.cpp" line="46"/>
+        <location filename="mxselectsound.cpp" line="82"/>
+        <location filename="mxselectsound.cpp" line="140"/>
+        <location filename="mxselectsound.cpp" line="152"/>
+        <location filename="mxselectsound.cpp" line="186"/>
+        <location filename="mxselectsound.cpp" line="195"/>
         <source>MX Select Sound</source>
         <translation>MX Karta dźwiękowa</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="28"/>
+        <location filename="mxselectsound.ui" line="28"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Your current default sound card is:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Obecna domyślna karta dźwiękowa to:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="64"/>
+        <location filename="mxselectsound.ui" line="64"/>
         <source>Display help </source>
         <translation>Wyświetl pomoc</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="67"/>
+        <location filename="mxselectsound.ui" line="67"/>
         <source>Help</source>
         <translation>Pomoc</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="75"/>
+        <location filename="mxselectsound.ui" line="75"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="104"/>
+        <location filename="mxselectsound.ui" line="104"/>
         <source>About this application</source>
         <translation>O programie</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="107"/>
+        <location filename="mxselectsound.ui" line="107"/>
         <source>About...</source>
         <translation>O...</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="115"/>
+        <location filename="mxselectsound.ui" line="115"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="147"/>
+        <location filename="mxselectsound.ui" line="147"/>
         <source>Quit application</source>
         <translation>Zakończ aplikację</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="150"/>
+        <location filename="mxselectsound.ui" line="150"/>
         <source>Close</source>
         <translation>Zamknij</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="158"/>
+        <location filename="mxselectsound.ui" line="158"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="199"/>
+        <location filename="mxselectsound.ui" line="199"/>
         <source>Apply</source>
         <translation>Zastosuj</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="222"/>
+        <location filename="mxselectsound.ui" line="222"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select another sound card if necessary:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wybierz inną kartę dźwiękową, jeśli to konieczne:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="248"/>
+        <location filename="mxselectsound.ui" line="248"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If your computer has more than one sound card, you can use this application to select the one to be default. &lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Jeśli komputer ma więcej niż jedną kartę dźwiękową, możesz użyć tej aplikacji do wybrania karty, która ma być domyślna.&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="294"/>
+        <location filename="mxselectsound.ui" line="294"/>
         <source>Test current sound card</source>
         <translation>Przetestuj obecną kartę dźwiękową</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="79"/>
+        <location filename="mxselectsound.cpp" line="83"/>
         <source>No sound cards/devices were found.</source>
         <translation>Nie znaleziono kart/urządzeń dźwiękowych.</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="91"/>
-        <location filename="../mxselectsound.cpp" line="105"/>
+        <location filename="mxselectsound.cpp" line="95"/>
+        <location filename="mxselectsound.cpp" line="109"/>
         <source>none</source>
         <translation>brak</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="135"/>
+        <location filename="mxselectsound.cpp" line="139"/>
         <source>About MX Select Sound</source>
         <translation>O programie MX Karta dźwiękowa</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="136"/>
+        <location filename="mxselectsound.cpp" line="140"/>
         <source>Version: </source>
         <translation>Wersja:</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="137"/>
+        <location filename="mxselectsound.cpp" line="141"/>
         <source>Program for selecting the default sound card in MX Linux</source>
         <translation>Program do wybierania domyślnej karty dźwiękowej w systemie MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="139"/>
+        <location filename="mxselectsound.cpp" line="143"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Prawa autorskie © MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="140"/>
-        <location filename="../mxselectsound.cpp" line="148"/>
+        <location filename="mxselectsound.cpp" line="144"/>
+        <location filename="mxselectsound.cpp" line="152"/>
         <source>License</source>
         <translation>Licencja</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="141"/>
+        <location filename="mxselectsound.cpp" line="145"/>
         <source>Changelog</source>
         <translation>Dziennik zmian</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="142"/>
+        <location filename="mxselectsound.cpp" line="146"/>
         <source>Cancel</source>
         <translation>Anuluj</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="157"/>
+        <location filename="mxselectsound.cpp" line="161"/>
         <source>&amp;Close</source>
         <translation>&amp;Zamknij</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="192"/>
+        <location filename="mxselectsound.cpp" line="196"/>
         <source>Could not play test sound.</source>
         <translation>Nie można odtworzyć dźwięku testowego.</translation>
     </message>
--- translations/mx-select-sound_sk.ts
+++ translations/mx-select-sound_sk.ts
@@ -1,143 +1,141 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="sk">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="2.1">
 <context>
     <name>mxselectsound</name>
     <message>
-        <location filename="../mxselectsound.ui" line="14"/>
-        <location filename="../mxselectsound.cpp" line="48"/>
-        <location filename="../mxselectsound.cpp" line="78"/>
-        <location filename="../mxselectsound.cpp" line="136"/>
-        <location filename="../mxselectsound.cpp" line="148"/>
-        <location filename="../mxselectsound.cpp" line="182"/>
-        <location filename="../mxselectsound.cpp" line="191"/>
+        <location filename="mxselectsound.ui" line="14"/>
+        <location filename="mxselectsound.cpp" line="46"/>
+        <location filename="mxselectsound.cpp" line="82"/>
+        <location filename="mxselectsound.cpp" line="140"/>
+        <location filename="mxselectsound.cpp" line="152"/>
+        <location filename="mxselectsound.cpp" line="186"/>
+        <location filename="mxselectsound.cpp" line="195"/>
         <source>MX Select Sound</source>
         <translation>MX Výber zvukov</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="28"/>
+        <location filename="mxselectsound.ui" line="28"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Your current default sound card is:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Momentálne používaná zvuková karta je:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="64"/>
+        <location filename="mxselectsound.ui" line="64"/>
         <source>Display help </source>
         <translation>Zobraziť nápovedu</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="67"/>
+        <location filename="mxselectsound.ui" line="67"/>
         <source>Help</source>
         <translation>Pomoc</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="75"/>
+        <location filename="mxselectsound.ui" line="75"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="104"/>
+        <location filename="mxselectsound.ui" line="104"/>
         <source>About this application</source>
         <translation>O tejto aplikácii</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="107"/>
+        <location filename="mxselectsound.ui" line="107"/>
         <source>About...</source>
         <translation>O Programe</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="115"/>
+        <location filename="mxselectsound.ui" line="115"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="147"/>
+        <location filename="mxselectsound.ui" line="147"/>
         <source>Quit application</source>
         <translation>Zatvoriť aplikáciu</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="150"/>
+        <location filename="mxselectsound.ui" line="150"/>
         <source>Close</source>
         <translation>Zatvoriť</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="158"/>
+        <location filename="mxselectsound.ui" line="158"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="199"/>
+        <location filename="mxselectsound.ui" line="199"/>
         <source>Apply</source>
         <translation>Použiť</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="222"/>
+        <location filename="mxselectsound.ui" line="222"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select another sound card if necessary:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Zvoľte inú zvukovú kartu:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="248"/>
+        <location filename="mxselectsound.ui" line="248"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If your computer has more than one sound card, you can use this application to select the one to be default. &lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Ak má Váš PC viac ako jednu zvukovú kartu, táto aplikácia vám umožní vybrať, ktorá z nich sa bude používať. &lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.ui" line="294"/>
+        <location filename="mxselectsound.ui" line="294"/>
         <source>Test current sound card</source>
         <translation>Otestovať používanú zvukovú kartu</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="79"/>
+        <location filename="mxselectsound.cpp" line="83"/>
         <source>No sound cards/devices were found.</source>
         <translation>Neboli nájdené žiadne karty/zariadenia.</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="91"/>
-        <location filename="../mxselectsound.cpp" line="105"/>
+        <location filename="mxselectsound.cpp" line="95"/>
+        <location filename="mxselectsound.cpp" line="109"/>
         <source>none</source>
         <translation>nič</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="135"/>
+        <location filename="mxselectsound.cpp" line="139"/>
         <source>About MX Select Sound</source>
         <translation>O MX Výber zvukov</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="136"/>
+        <location filename="mxselectsound.cpp" line="140"/>
         <source>Version: </source>
         <translation>Verzia:</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="137"/>
+        <location filename="mxselectsound.cpp" line="141"/>
         <source>Program for selecting the default sound card in MX Linux</source>
         <translation>Program pre výber zvukovej karty pre MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="139"/>
+        <location filename="mxselectsound.cpp" line="143"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="140"/>
-        <location filename="../mxselectsound.cpp" line="148"/>
+        <location filename="mxselectsound.cpp" line="144"/>
+        <location filename="mxselectsound.cpp" line="152"/>
         <source>License</source>
         <translation>Licencia</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="141"/>
+        <location filename="mxselectsound.cpp" line="145"/>
         <source>Changelog</source>
         <translation>História zmien</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="142"/>
+        <location filename="mxselectsound.cpp" line="146"/>
         <source>Cancel</source>
         <translation>Zrušiť</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="157"/>
+        <location filename="mxselectsound.cpp" line="161"/>
         <source>&amp;Close</source>
         <translation>&amp;Zatvoriť</translation>
     </message>
     <message>
-        <location filename="../mxselectsound.cpp" line="192"/>
+        <location filename="mxselectsound.cpp" line="196"/>
         <source>Could not play test sound.</source>
         <translation>Testovací zvuk sa nepodarilo prehrať.</translation>
     </message>
